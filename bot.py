d935facff686b8a5f288a809ec5597fe8dcbe561
James Stockwell Mendoza <jstockwellmendoza@gmail.com>
2023-02-02 18:38:29 +0100

    stuff

--- bot.py
+++ bot.py
@@ -1,3 +1,45 @@
+# Created by: @jstockwell on GitHub
+# GNU GENERAL PUBLIC LICENSE
+# Version 3, 29 June 2007
+
+# Copyright © 2007 Free Software Foundation, Inc. <https://fsf.org/>
+
+# Everyone is permitted to copy and distribute verbatim copies of this license document, but changing it is not allowed.
+
+# The GNU General Public License is a free, copyleft license for software and other kinds of works.
+
+# The licenses for most software and other practical works are designed to take away your freedom to share and change the works. 
+# By contrast, the GNU General Public License is intended to guarantee your freedom to share and change all versions of a program
+# to make sure it remains free software for all its users. We, the Free Software Foundation, use the GNU General Public License for most of our software; 
+# it applies also to any other work released this way by its authors. You can apply it to your programs, too.
+
+# When we speak of free software, we are referring to freedom, not price. Our General Public Licenses are designed to make sure that you have the freedom 
+# to distribute copies of free software (and charge for them if you wish), that you receive source code or can get it if you want it, that you can change 
+# the software or use pieces of it in new free programs, and that you know you can do these things.
+
+# To protect your rights, we need to prevent others from denying you these rights or asking you to surrender the rights. Therefore, you have certain 
+# responsibilities if you distribute copies of the software, or if you modify it: responsibilities to respect the freedom of others.
+
+# For example, if you distribute copies of such a program, whether gratis or for a fee, you must pass on to the recipients the same freedoms that you 
+# received. You must make sure that they, too, receive or can get the source code. And you must show them these terms so they know their rights.
+
+# Developers that use the GNU GPL protect your rights with two steps: (1) assert copyright on the software, and (2) offer you this License giving you 
+# legal permission to copy, distribute and/or modify it.
+
+# For the developers' and authors' protection, the GPL clearly explains that there is no warranty for this free software. For both users' 
+# and authors' sake, the GPL requires that modified versions be marked as changed, so that their problems will not be attributed erroneously 
+# to authors of previous versions.
+
+# Some devices are designed to deny users access to install or run modified versions of the software inside them, although the manufacturer 
+# can do so. This is fundamentally incompatible with the aim of protecting users' freedom to change the software. The systematic pattern of 
+# such abuse occurs in the area of products for individuals to use, which is precisely where it is most unacceptable. Therefore, we have 
+# designed this version of the GPL to prohibit the practice for those products. If such problems arise substantially in other domains, we 
+# stand ready to extend this provision to those domains in future versions of the GPL, as needed to protect the freedom of users.
+
+# Finally, every program is threatened constantly by software patents. States should not allow patents to restrict development and use of 
+# software on general-purpose computers, but in those that do, we wish to avoid the special danger that patents applied to a free program 
+# could make it effectively proprietary. To prevent this, the GPL assures that patents cannot be used to render the program non-free.
+
 import os
 import discord
 import requests
@@ -21,12 +63,13 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 TEST_TOKEN = os.getenv('TEST_TOKEN')
 SRCOM_TOKEN = os.getenv('SRCOM_TOKEN')
 TINYDB_PATH = os.getenv('TINYDB_PATH')
+GUIDELINES_CHANNEL = os.getenv('GUIDELINES_CHANNEL')
 DEV_MODE = True
 
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 bot.remove_command('help')
 # TODO Automate game_db creation
-game_db = json.load(open('games.json', 'r'))
+game_db = json.load(open('json/games.json', 'r'))
 db = TinyDB(TINYDB_PATH)
 
 ### --- Functions --- ###
@@ -55,6 +98,10 @@ async def on_command_error(ctx, error):
 
     print(error)
     await ctx.send(message)
+
+@bot.event
+async def on_reaction_add(reaction, user):
+    gender_role(reaction, user)
 
 class switch(object):
     value = None
@@ -260,16 +307,9 @@ async def post_run(channel_id, run, title):
     channel = bot.get_channel(channel_id)
     await channel.send(embed=embed)
 
-# Change to your desired channels
-channel_lookup = {
-    "o1yrkr6q": 155845329957158912,
-    "9j1l8v6g": 155844832185548800,
-    "y6545p8d": 155844832185548800,
-    "j1nvyx6p": 155845509146345473
-}
-
 @tasks.loop(seconds = 300) # repeat after every five minutes
 async def post_verification():
+    channel_lookup = json.load(open('json/channels.json', 'r'))
     notifications = requests.get(f"{base_url}notifications", headers={"X-API-Key": SRCOM_TOKEN}).json()["data"]
     for notification in notifications:
         notif_time = dt.strptime(notification['created'].replace('T',' ').replace('Z',''), '%Y-%m-%d %H:%M:%S')
@@ -284,6 +324,7 @@ async def post_verification():
                 else:
                     await post_run(channel_id, run['data'], "Run Verified!")
 
+# TODO Finish this
 # Link SRC account to a Discord account
 @bot.command(name="src", help="Set your SRC account")
 async def src(ctx, *args):
@@ -311,6 +352,11 @@ async def validate_user(discord_id, account):
 #        table.insert({'discord': ctx.author.id, 'src': account})
 #    else:
 #        table.update({'src': account}, Query().discord == ctx.author.id)
+
+async def gender_role(reaction, user):
+    channel = bot.get_channel(GUIDELINES_CHANNEL)
+
+
 
 # TODO Ideas
 #
